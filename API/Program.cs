897111dde547e2df204eb355b1cc2e69a86abb69
Jeremy <jeremybusuttil5@gmail.com>
2023-09-12 22:30:30 +0200

    End of section 3 - Walking skeleton complete

--- API/Program.cs
+++ API/Program.cs
@@ -14,6 +14,10 @@ builder.Services.AddDbContext<DataContext>(opt =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options => {
+    options.AddDefaultPolicy(policy => policy.AllowAnyHeader()
+        .AllowAnyMethod().WithOrigins("https://localhost:4200"));
+});
 
 var app = builder.Build();
 
@@ -27,6 +31,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors();
 
 app.MapControllers();
 
