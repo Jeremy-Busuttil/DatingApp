62d1f6cc7f4e8b2dcc90f1cdbd6d7269a58ab648
Jeremy <jeremybusuttil5@gmail.com>
2023-11-14 19:40:13 +0100

    End of section 9

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, map } from 'rxjs';
 import { User } from '../_models/user';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ import { User } from '../_models/user';
 export class AccountService {
   private currentUserSource = new BehaviorSubject<User | null>(null);
 
-  baseUrl = 'https://localhost:5001/api/';
+  baseUrl = environment.apiUrl;
   currentUser$ = this.currentUserSource.asObservable();
 
   constructor(private http: HttpClient) { }
